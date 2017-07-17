eb7bdb023d561fdf8420e3213c45727e9b8a54c5
thislight <l1589002388@gmail.com>
2017-07-17 20:25:25 +0800

    Add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+VERSION = "0.1.0-alpha.0"
+
+
+setup(
+        name="paperdb",
+        version=VERSION,
+        author="thislight",
+        author_email="l1589002388@gmail.com",
+        packages=["paperdb"],
+        )
+
